5422877af8008cee72c1176939401250b08dc33c
cjwalle97 <cjwalle97@gmail.com>
2017-01-11 16:48:28 -0600

    Initial Commit
    Initial Commit

---
+++ WindForms/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(127, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(400, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 203);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(631, 54);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(127, 136);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(400, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(127, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(400, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 423);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ WindForms/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //class Player
+        //{
+        //    private int m_level;
+        //    private int m_health;
+        //    private int m_mana;
+        //    private int m_strength;
+        //    private int m_magic;
+        //    private int m_dexterity;
+        //    private int m_agility;
+        //    private int m_resilience;
+
+        //    public Player (int l, int h, int m, int s, int b, int d, int a, int r)
+        //    {
+        //        m_level = l; m_health = h; m_mana = m; m_strength = s;
+        //        m_magic = b; m_dexterity = d; m_agility = a; m_resilience = r;
+        //    }
+        //    public int Level;
+        //    public int HP;
+        //    public int MP;
+        //    public int STR;
+        //    public int MAG;
+        //    public int DEX;
+        //    public int AGI;
+        //    public int RES;
+        //    public Player LevelUp()
+        //    {
+        //        return Player 
+        //    }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (progressBar1.Value == 100)
+            {
+                textBox1.Text = "LEVEL UP";
+                progressBar1.Value -= 100;
+            }
+            if (progressBar1.Value < 100)
+            {
+                if (progressBar1.Value < 60)
+                {
+                    progressBar1.Value += 10;
+                }
+                if (progressBar1.Value >= 60)
+                {
+                    progressBar1.Value += 5;
+                }
+            }
+        }
+
+    }
+}
